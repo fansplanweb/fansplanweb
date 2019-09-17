e2f9da493afdc74173f7b2419b23f793a8bb5d50
deepakuoutsourcingtech <deepaku.outsourcingtech@gmail.com>
2019-09-17 17:58:26 +0530

    delete file

--- test_url.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-// echo $url = base64_decode("aHR0cDovL3d3dy5zbXNhbGVydC5jby5pbi9hcGkvcHVzaC5qc29u");
- echo $url = base64_decode("aHR0cDovL3d3dy5zbXNhbGVydC5jby5pbi9hcGkvbXZlcmlmeS5qc29u==");
- echo $url = base64_decode("aHR0cDovL3d3dy5zbXNhbGVydC5jby5pbi9hcGkvdGVtcGxhdGVsaXN0Lmpzb24==");
-?>
